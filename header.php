6edc7d8c16c876833a315646404d0136aafba920
Jones-bass <jonesbass.tb@gmail.com>
2022-08-20 10:00:40 -0300

    Add CSS Javascript

--- header.php
+++ header.php
@@ -2,21 +2,25 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title><?php bloginfo('name'); ?></title>
-		<meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
+		<?php if(is_home() && !is_front_page()) { ?>
+		<title><?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo', get_option('page_for_posts')); ?></title>
+		<?php } else { ?>
+		<title><?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?></title>
+		<?php } ?>		
+		<meta name="description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>">
 
 		<meta property="og:type" content="website"/>
-		<meta property="og:title" content="<?php bloginfo('name'); ?>"/>
-		<meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
-		<meta property="og:url" content="http://bikcraft.com"/>
-		<meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
+		<meta property="og:title" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
+		<meta property="og:description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>"/>
+		<meta property="og:url" content="<?php bloginfo('url'); ?>"/>
+		<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/og-image.png"/>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="shortcut icon" href="favicon.ico">
 
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-		<script src="js/libs/modernizr.custom.45655.js"></script>
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<script>document.documentElement.classList.add("js");</script>
 
 		<?php wp_head(); ?>
 		
@@ -28,13 +32,15 @@
 				<a href="/bikecraft/" class="grid-4">
 					<img src="<?php echo get_template_directory_uri(); ?>/img/bikcraft.png" alt="Bikcraft">
 				</a>
-				<nav class="grid-12 header_menu">
-					<ul>
-						<li><a href="/bikecraft/sobre/">Sobre</a></li>
-						<li><a href="/bikecraft/produtos/">Produtos</a></li>
-						<li><a href="/bikecraft/portifolio/">Portfólio</a></li>
-						<li><a href="/bikecraft/contato/">Contato</a></li>
-					</ul>
+					<nav class="grid-12 header_menu">
+					<?php
+						$args = array(
+							'menu' => 'principal',
+							'theme_location' => 'menu-principal',
+							'container' => false
+						);
+						wp_nav_menu( $args );
+					?>
 				</nav>
 			</div>
 		</header>
--- page-home.php
+++ page-home.php
@@ -1,10 +1,9 @@
+<?php
+// Template Name: Home
+get_header();
+?>
 
-	<?php
-	// template Name: Home 
-	get_header();
-	?>
-	
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <?php
 	$imagem_id = get_field('background_home');
@@ -13,15 +12,15 @@
 ?>
 
 <style type="text/css">
-		.introducao {
-		background: url('<?php echo $background_large[0] ?>') no-repeat center;
-		background-size: cover;
-	}
-
-	@media only screen and (max-width: 767px) {
-		.introducao {
-		background: url('<?php echo $background_medium[0] ?>') no-repeat center;
-		background-size: cover;
+.introducao {
+	background: url('<?php echo $background_large[0] ?>') no-repeat center;
+	background-size: cover;
+}
+@media only screen and (max-width: 767px) {
+.introducao {
+	background: url('<?php echo $background_medium[0] ?>') no-repeat center;
+	background-size: cover;
+}
 }
 </style>
 
@@ -32,7 +31,7 @@
 					<p><?php the_field('quote_introducao'); ?></p>
 					<cite><?php the_field('citacao_introducao'); ?></cite>
 				</blockquote>
-				<a href="produtos.html" class="btn">Orçamento</a>
+				<a href="/bikcraft/produtos/" class="btn">Orçamento</a>
 			</div>
 		</section>
 		
@@ -40,30 +39,28 @@
 			<h2 class="subtitulo">Produtos</h2>
 			<ul class="produtos_lista">
 
-				<li class="grid-1-3">
-					<div class="produtos_icone">
-						<img src="img/produtos/passeio.png" alt="Bikcraft Passeio">
-					</div>
-					<h3>Passeio</h3>
-					<p>Muito melhor do que passear pela orla a vidros fechados.</p>
-				</li>
+				<?php
+					$args = array (
+							'post_type' => 'produtos',
+							'order'   => 'ASC'
+						);
+						$the_query = new WP_Query ( $args );
+				?>
+
+				<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
 				<li class="grid-1-3">
+					<a href="<?php the_permalink(); ?>">
 					<div class="produtos_icone">
-						<img src="img/produtos/esporte.png" alt="Bikcraft Esporte">
+						<img src="<?php the_field('icone_produto'); ?>" alt="Bikcraft Passeio">
 					</div>
-					<h3>Esporte</h3>
-					<p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
+					<h3><?php the_title(); ?></h3>
+					<p><?php the_field('resumo_produto'); ?></p>
+					</a>
 				</li>
 
-				<li class="grid-1-3">
-					<div class="produtos_icone">
-						<img src="img/produtos/retro.png" alt="Bikcraft Retrô">
-					</div>
-					<h3>Retrô</h3>
-					<p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
-				</li>
-
+				<?php endwhile; else: endif; ?>
+				<?php wp_reset_query(); wp_reset_postdata(); ?>
 			</ul>
 
 			<div class="call">
@@ -73,15 +70,15 @@
 
 		</section>
 		<!-- Fecha Produtos -->
+
 		<section class="portfolio">
 			<div class="container">
 				<h2 class="subtitulo">Portfólio</h2>
-				<?php include(TEMPLATEPATH . "/inc/clientPortifolio.php"); ?>
-
+				<?php include(TEMPLATEPATH . "/inc/clientes-portfolio.php"); ?>
 			</div>
 		</section>
 
 		<?php include(TEMPLATEPATH . "/inc/qualidade.php"); ?>
-	<?php endwhile; else: endif; ?>
+<?php endwhile; else: endif; ?>
 
-		<?php get_footer(); ?>
+<?php get_footer(); ?>
